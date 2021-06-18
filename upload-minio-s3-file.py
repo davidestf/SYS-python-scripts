580f24152f3aa87d13807612c3f8b21255b993c2
Davide staffieri <69149868+dstf@users.noreply.github.com>
2021-06-18 15:12:21 +0100

    Create upload-minio-s3-file.py

---
+++ upload-minio-s3-file.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env/python
+import boto3
+from botocore.client import Config
+
+
+s3 = boto3.resource('s3',
+                    endpoint_url='http://minio.staffieri.co.uk:9000',
+                    aws_access_key_id='minio',
+                    aws_secret_access_key='minio123',
+                    config=Config(signature_version='s3v4'),
+                    region_name='us-east-1')
+
+
+
+## upload a file from local file system 
+s3.Bucket('test').upload_file('/root/miniotest/miniotest.txt','miniotest.txt')
+
+# download the object  and save it to local FS 
+s3.Bucket('test').download_file('miniotest.txt', '/tmp/miniotestdownload.txt')
+
+print ("successfull")
+
